2e44507125853d9f5639dd4960196e3640d64f29
xhr <2435540531@qq.com>
2024-12-03 19:44:19 +0800

    修改启动日志，增加项目前缀/member，修改logback日志

--- member/src/main/java/com/lyc/member/MemberApplication.java
+++ member/src/main/java/com/lyc/member/MemberApplication.java
@@ -1,14 +1,23 @@
 package com.lyc.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author lyc
  */
 @SpringBootApplication
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+//        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication application = new SpringApplication(MemberApplication.class);
+        final ConfigurableEnvironment environment = application.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+        LOG.info("测试地址\thttp://127.0.0.1:{}/member/hello", environment.getProperty("server.port"));
     }
 }
