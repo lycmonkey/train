f279dcf44696128b514469699c579e0b9072ddb9
xhr <2435540531@qq.com>
2024-12-06 09:56:56 +0800

    增加控台主页

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
+/*
   {
     path: '/',
     name: 'home',
@@ -13,13 +13,19 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/!* webpackChunkName: "about" *!/ '../views/AboutView.vue')
   },
+*/
 
   {
     path: '/login',
     name: 'login',
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/',
+    name: 'main',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Main.vue')
   }
 
 ]
