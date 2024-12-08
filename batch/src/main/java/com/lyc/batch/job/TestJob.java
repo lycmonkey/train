995ccc9c8c74a9fcaf8852103a13c3af59436d18
xhr <2435540531@qq.com>
2024-12-08 19:22:02 +0800

    禁止任务并发执行

--- batch/src/main/java/com/lyc/batch/job/TestJob.java
+++ batch/src/main/java/com/lyc/batch/job/TestJob.java
@@ -1,5 +1,6 @@
 package com.lyc.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -7,10 +8,20 @@ import org.quartz.JobExecutionException;
 /**
  * @author lyc
  */
+// 禁止并发执行任务
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         System.out.println("build success");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("build twice");
+
+
     }
 }
